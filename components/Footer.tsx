36fab2729b175c03ad62dd7ed0b5da3f158b2f7c
Udoka <udochukwukaonyela@gmail.com>
2022-11-08 22:03:20 +0100

    created footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import  React, { FC } from 'react';
+
+export interface IFooterProps {
+}
+
+const Footer: FC<IFooterProps> = (props) => {
+  return (
+    <section>
+      Footer
+    </section>
+  );
+}
+export default Footer;
