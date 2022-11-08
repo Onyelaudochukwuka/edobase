9c58b451d772c916c44daf059ceb280bb86c7b3b
Udoka <udochukwukaonyela@gmail.com>
2022-11-08 22:00:32 +0100

    created Navbar Component

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import  React, { FC } from 'react';
+
+export interface INavbarProps {
+}
+
+const Navbar: FC<INavbarProps> = (props) => {
+  return (
+    <section>
+      Navbar
+    </section>
+  );
+}
+export default Navbar;
