71c9ec2e7b378214efe1cfcc8a976caa1d83503e
Udoka <udochukwukaonyela@gmail.com>
2022-11-10 21:53:43 +0100

    created navlink component

---
+++ components/atoms/NavLink.tsx
@@ -0,0 +1,13 @@
+import  React, { FC } from 'react';
+
+export interface INavLinkProps {
+}
+
+const NavLink: FC<INavLinkProps> = (props) => {
+  return (
+    <section>
+      NavLink
+    </section>
+  );
+}
+export default NavLink;
