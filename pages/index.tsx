04c99d3de3bc223e388d4e2e6bad8b6bb194d1d2
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2022-11-29 20:21:38 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,7 @@ const Home: NextPage = () => {
         <title>Create Next App</title>
          <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+   <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
