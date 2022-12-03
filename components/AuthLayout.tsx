d92cb6d41ed46e804db0616548353d51521ef938
Udoka <udochukwukaonyela@gmail.com>
2022-12-03 11:19:28 +0100

    feat: added a link to home

--- components/AuthLayout.tsx
+++ components/AuthLayout.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 
+import Link from 'next/link';
+
 import { Home } from '../assets';
 
 import { AuthSidebar } from '.';
@@ -16,8 +18,13 @@ const AuthLayout: FC<IAuthLayoutProps> = ({
 }) => (
   <section className="flex min-h-screen">
     <AuthSidebar className="basis-5/12" />
-    <div className={`${className} basis-7/12 py-6 px-12 flex flex-col justify-between`} {...props}>
-      <Home />
+    <div
+      className={`${className} lg:basis-7/12 w-full py-6 px-12 flex flex-col justify-between`}
+      {...props}
+    >
+      <Link href="/">
+        <Home />
+      </Link>
       {children}
       <div className="flex justify-between">
         <span>&copy;2022 edobase</span>
