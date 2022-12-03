0dc30de9351579b45dbc223df1eff06fc51415ea
Udoka <udochukwukaonyela@gmail.com>
2022-12-03 09:00:36 +0100

    styling

--- components/AuthSidebar.tsx
+++ components/AuthSidebar.tsx
@@ -1,13 +1,19 @@
 import React, { FC } from 'react';
 
+import { AuthBlur } from '../assets';
+
 export interface IAuthSidebarProps {
   className: string
 }
 
 const AuthSidebar: FC<IAuthSidebarProps> = ({ className, ...props }) => (
-  <section className={`${className} gradient`} {...props}>
-    df
+  <section
+    className={`${className} gradient grid place-items-center`}
+    {...props}
+  >
+    <div className="gradient-child w-3/4 h-1/2 p-4">df</div>
+    <AuthBlur className="absolute bottom-0 left-0" />
   </section>
-)
+);
 
 export default AuthSidebar;
