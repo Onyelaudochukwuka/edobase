819e5c49312f67268fc652be0eae6d22ed5a90c5
Udoka <udochukwukaonyela@gmail.com>
2022-12-03 08:35:25 +0100

    exported icons

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,6 @@
-export { default as Navbar } from "./Navbar";
-export { default as Layout } from "./Layout";
-export { default as Footer } from "./Footer";
-export { default as Sidebar } from "./Sidebar";
+export { default as AuthSidebar } from './AuthSidebar';
+export { default as AuthLayout } from './AuthLayout';
+export { default as Navbar } from './Navbar';
+export { default as Layout } from './Layout';
+export { default as Footer } from './Footer';
+export { default as Sidebar } from './Sidebar';
