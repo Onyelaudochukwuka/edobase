cb2718ff5d0e9b50052b4d5824b1df040a85201c
Udoka <udochukwukaonyela@gmail.com>
2022-12-03 09:09:05 +0100

    addd properties

--- components/AuthSidebar.tsx
+++ components/AuthSidebar.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-import { AuthBlur } from '../assets';
+import { AuthBlur, AuthPointer } from '../assets';
 
 export interface IAuthSidebarProps {
   className: string
@@ -11,7 +11,17 @@ const AuthSidebar: FC<IAuthSidebarProps> = ({ className, ...props }) => (
     className={`${className} gradient grid place-items-center`}
     {...props}
   >
-    <div className="gradient-child w-3/4 h-1/2 p-4">df</div>
+    <div className="gradient-child w-3/4 h-1/2 p-2 z-20">
+      <div className="flex items-center">
+        <AuthPointer className="w-20" />
+        <span className="text-5xl text-white font-black">Edobase</span>
+      </div>
+      <p>
+        Platform for getting the latest
+        {' '}
+        <span className="text-loginHighlight">information.</span>
+      </p>
+    </div>
     <AuthBlur className="absolute bottom-0 left-0" />
   </section>
 );
