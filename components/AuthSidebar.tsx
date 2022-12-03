24ffcb36f77b3d0f459e3114d73050e7ac8fc2ee
Udoka <udochukwukaonyela@gmail.com>
2022-12-03 08:35:19 +0100

    created auth sideba

---
+++ components/AuthSidebar.tsx
@@ -0,0 +1,13 @@
+import  React, { FC } from 'react';
+
+export interface IAuthSidebarProps {
+}
+
+const AuthSidebar: FC<IAuthSidebarProps> = (props) => {
+  return (
+    <section>
+      AuthSidebar
+    </section>
+  );
+}
+export default AuthSidebar;
