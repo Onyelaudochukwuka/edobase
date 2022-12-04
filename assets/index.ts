dcc3b514aa36e1e8fa7ef8d08dc8757b0889a57c
Udoka <udochukwukaonyela@gmail.com>
2022-12-04 17:22:07 +0100

    chore: exported arrow icon

--- assets/index.ts
+++ assets/index.ts
@@ -3,3 +3,4 @@ export { default as AuthPointer } from './AuthPointer.icon';
 export { default as AuthBlur } from './AuthBlur.icon';
 export { default as Check } from './Check.icon';
 export { default as Key } from './Key.icon';
+export { default as Arrow } from './Arrow.icon';
