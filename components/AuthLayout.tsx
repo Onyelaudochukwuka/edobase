426940520716578faeeb8298ec1610631de4bfb2
Udoka <udochukwukaonyela@gmail.com>
2022-12-04 19:26:06 +0100

    feat: added page change animations

--- components/AuthLayout.tsx
+++ components/AuthLayout.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 
 import { Home } from '../assets';
@@ -17,30 +18,43 @@ const AuthLayout: FC<IAuthLayoutProps> = ({
   className,
   nosidebar,
   ...props
-}) => (
-  <section className="flex min-h-screen">
-    <AuthSidebar
-      className={`basis-5/12 ${
-        nosidebar ?? false ? 'hidden' : 'lg:grid hidden place-items-center'
-      }`}
-    />
-    <div
-      className={`${className} ${
-        nosidebar ?? false ? 'basis-full' : 'lg:basis-7/12'
-      } w-full py-6 px-12 flex flex-col justify-between`}
-      {...props}
+}) => {
+  const { section: Section } = motion;
+  return (
+    <Section
+      initial={{ x: -100, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{
+        duration: 0.5,
+        type: 'spring',
+        stiffness: 120,
+        damping: 20,
+      }}
+      className="flex min-h-screen"
     >
-      <Link href="/">
-        <Home />
-      </Link>
-      {children}
-      <div className="flex justify-between">
-        <span>&copy;2022 edobase</span>
-        <span>privacy policy</span>
+      <AuthSidebar
+        className={`basis-5/12 ${
+          nosidebar ?? false ? 'hidden' : 'lg:grid hidden place-items-center'
+        }`}
+      />
+      <div
+        className={`${className} ${
+          nosidebar ?? false ? 'basis-full' : 'lg:basis-7/12'
+        } w-full py-6 px-12 flex flex-col justify-between`}
+        {...props}
+      >
+        <Link href="/">
+          <Home />
+        </Link>
+        {children}
+        <div className="flex justify-between">
+          <span>&copy;2022 edobase</span>
+          <span>privacy policy</span>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </Section>
+  );
+};
 AuthLayout.defaultProps = {
   nosidebar: false,
 };
