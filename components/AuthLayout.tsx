af12007f8246238a0a48a173d7843c8de02c6bdb
Udoka <udochukwukaonyela@gmail.com>
2022-12-04 17:08:03 +0100

    added conditional rendering logi

--- components/AuthLayout.tsx
+++ components/AuthLayout.tsx
@@ -7,19 +7,27 @@ import { Home } from '../assets';
 import { AuthSidebar } from '.';
 
 export interface IAuthLayoutProps {
-  children: React.ReactNode
-  className: string
+  children: React.ReactNode;
+  className: string;
+  nosidebar?: boolean;
 }
 
 const AuthLayout: FC<IAuthLayoutProps> = ({
   children,
   className,
+  nosidebar,
   ...props
 }) => (
   <section className="flex min-h-screen">
-    <AuthSidebar className="basis-5/12" />
+    <AuthSidebar
+      className={`basis-5/12 ${
+        nosidebar ?? false ? 'hidden' : 'lg:grid hidden place-items-center'
+      }`}
+    />
     <div
-      className={`${className} lg:basis-7/12 w-full py-6 px-12 flex flex-col justify-between`}
+      className={`${className} ${
+        nosidebar ?? false ? 'basis-full' : 'lg:basis-7/12'
+      } w-full py-6 px-12 flex flex-col justify-between`}
       {...props}
     >
       <Link href="/">
@@ -33,5 +41,7 @@ const AuthLayout: FC<IAuthLayoutProps> = ({
     </div>
   </section>
 );
-
+AuthLayout.defaultProps = {
+  nosidebar: false,
+};
 export default AuthLayout;
