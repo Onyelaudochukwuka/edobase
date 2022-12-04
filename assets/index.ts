4f2007608a9ecb98f667ff9c0eee66bb8ee4ad6f
Udoka <udochukwukaonyela@gmail.com>
2022-12-04 17:09:22 +0100

    exported icons

--- assets/index.ts
+++ assets/index.ts
@@ -2,3 +2,4 @@ export { default as Home } from './Home.icon';
 export { default as AuthPointer } from './AuthPointer.icon';
 export { default as AuthBlur } from './AuthBlur.icon';
 export { default as Check } from './Check.icon';
+export { default as Key } from './Key.icon';
