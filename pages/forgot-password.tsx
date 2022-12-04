60175f3d8c21f8a8a09419a6e740e4bfeedb9acf
Udoka <udochukwukaonyela@gmail.com>
2022-12-04 20:10:52 +0100

    fat: fixes

--- pages/forgot-password.tsx
+++ pages/forgot-password.tsx
@@ -10,7 +10,7 @@ const Forgotpassword: NextPage = () => {
   const [email, setEmail] = useState('');
   return (
     <AuthLayout nosidebar className="">
-      <div className="flex flex-col gap-6 w-1/2 m-auto items-center">
+      <div className="flex flex-col gap-6 lg:w-1/2 w-11/12 m-auto items-center">
         <span className="flex flex-col gap-8 items-center">
           <Key />
           <h1 className="text-3xl font-bold">Forgot Password</h1>
