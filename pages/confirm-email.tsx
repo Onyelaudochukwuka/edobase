e7c1a264419eb8556b3a5761c560f3a323710091
Udoka <udochukwukaonyela@gmail.com>
2022-12-05 19:44:04 +0100

    feat: created confirm image page

---
+++ pages/confirm-email.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import type { NextPage } from 'next';
+
+const confirmEmail: NextPage = () => {
+  return <section>confirm-email</section>;
+};
+export default confirmEmail;
