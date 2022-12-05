843933ae85f9bb509867b64fb51626763ebce377
Udoka <udochukwukaonyela@gmail.com>
2022-12-05 19:45:33 +0100

    feat: added eleements

--- pages/confirm-email.tsx
+++ pages/confirm-email.tsx
@@ -1,7 +1,37 @@
 import React, { useState } from 'react';
 import type { NextPage } from 'next';
+import { AuthLayout, PlaceholderInput } from "../components";
+import Link from "next/link";
 
 const confirmEmail: NextPage = () => {
-  return <section>confirm-email</section>;
+  const [email, setEmail] = useState('');
+  return (
+    <AuthLayout nosidebar className="">
+      <div className="flex flex-col gap-6 lg:w-1/2 w-full m-auto items-center">
+        <span className="flex flex-col gap-8 items-center">
+          {/* <Key /> */}
+          <h1 className="text-3xl font-bold">Forgot Password</h1>
+          <p className="text-placeholder font-bold lg:text-lg md:text-base text-sm">
+            No worries, we’ll send you reset instructions
+          </p>
+        </span>
+        <form className="flex flex-col gap-6 w-full">
+          <div className="flex flex-col gap-6">
+            <PlaceholderInput
+              {...{
+                type: 'email',
+                placeholder: 'Email',
+                state: email,
+                setState: setEmail,
+              }}
+            />
+          </div>
+          <button type="submit" className="auth-btn py-4 text-white font-black">
+            Reset Password
+          </button>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 };
 export default confirmEmail;
