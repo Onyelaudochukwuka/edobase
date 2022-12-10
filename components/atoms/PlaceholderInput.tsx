7a6b85418cb6ed453b7973aacc68d51b26a14f74
Udoka <udochukwukaonyela@gmail.com>
2022-12-10 11:04:15 +0100

    added generic and elaborate typing

--- components/atoms/PlaceholderInput.tsx
+++ components/atoms/PlaceholderInput.tsx
@@ -1,29 +1,34 @@
-import React, { Dispatch, FC, SetStateAction } from 'react';
+import React, {
+  Dispatch,
+  InputHTMLAttributes,
+  ReactElement,
+  SetStateAction,
+} from 'react';
 
-export interface IPlaceholderInputProps {
-  type: string
-  placeholder: string
-  state: string
-  setState: Dispatch<SetStateAction<string>>
+interface IPlaceholderInputProps<T> {
+  type: InputHTMLAttributes<HTMLInputElement>['type']
+  placeholder: InputHTMLAttributes<HTMLInputElement>['placeholder']
+  state: InputHTMLAttributes<HTMLInputElement>['value']
+  setState: Dispatch<SetStateAction<T>>
 }
 
-const PlaceholderInput: FC<IPlaceholderInputProps> = ({
+const PlaceholderInput = <T extends unknown>({
   type,
   placeholder,
   state,
   setState,
   ...props
-}) => (
+}: IPlaceholderInputProps<T>): ReactElement => (
   <input
     {...props}
     {...{
       type,
       placeholder,
       value: state,
-      onChange: (e) => setState(e.target.value),
+      onChange: (e) => setState((typeof state === 'number' ? Number(e.target.value) : e.target.value) as T),
     }}
     className="border-b-2 outline-none focus:outline-none border-action/50 focus:border-action/100 transition-colors duration-300 ease-in px-1 py-3"
   />
-);
+  );
 
 export default PlaceholderInput;
