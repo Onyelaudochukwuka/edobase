5cec435030e4e14339fe4dece02034b439751ce0
Udoka <udochukwukaonyela@gmail.com>
2022-12-11 17:12:02 +0100

    feat: added default props

--- components/atoms/NavLink.tsx
+++ components/atoms/NavLink.tsx
@@ -1,13 +1,18 @@
-import Link from "next/link";
 import React, { FC, ReactNode } from 'react';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
+import Link from 'next/link';
+
 export interface INavLinkProps {
-    to: string
-    external?: boolean
-    children: ReactNode
+  to: string
+  external?: boolean
+  children: ReactNode
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const NavLinks: FC<INavLinkProps> = ({ to, external, children }) => <Link href={to}>{ children }</Link>;
-export default NavLinks;
+const NavLink: FC<INavLinkProps> = ({ to, external, children }) => (
+  <Link href={to}>{children}</Link>
+);
+NavLink.defaultProps = {
+    external: false,
+};
+export default NavLink;
