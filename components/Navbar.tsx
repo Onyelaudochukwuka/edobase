4f170421bfa82c5094b86d075e0fc08d935ff852
Udoka <udochukwukaonyela@gmail.com>
2022-12-13 20:44:37 +0100

    chore: added className interference

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface INavbarProps {}
+export interface INavbarProps {
+    className?: string;
+}
 
 const Navbar: FC<INavbarProps> = () => <nav>Navbar</nav>;
 export default Navbar;
