c30dbcc27df0b5a96c5376229fbeb4f11d90939d
Udoka <udochukwukaonyela@gmail.com>
2022-12-13 20:44:45 +0100

    chore: added className interference

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface IFooterProps {}
+export interface IFooterProps {
+    className?: string;
+}
 
 const Footer: FC<IFooterProps> = () => <footer>Footer</footer>;
 export default Footer;
