75366d99722af5a9364bab9db2cb3e227e7c280c
Udoka <udochukwukaonyela@gmail.com>
2022-12-13 20:44:49 +0100

    chore: added className interference

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ISidebarProps {}
+export interface ISidebarProps {
+    className?: string;
+}
 
 const Sidebar: FC<ISidebarProps> = () => <aside>Sidebar</aside>;
 export default Sidebar;
