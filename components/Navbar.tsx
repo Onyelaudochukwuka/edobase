471f3c2324720fda07ff222dc2909cd020bdeaac
Udoka <udochukwukaonyela@gmail.com>
2022-12-18 12:15:38 +0100

    feat: added elements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,25 @@
-import React, { FC, useState } from 'react';
+import React, {
+  FC,
+} from 'react';
 
 export interface INavbarProps {
   className: string
 }
 
 const Navbar: FC<INavbarProps> = ({ className }) => {
-  const state = useState();
   return (
-    <nav className={`${className} `}>
-      <h1>Edobase</h1>
+    <nav className={`${className} bg-primary h-fit py-3`}>
+      <h1 className="text-center font-bold text-black text-xl">Edobase</h1>
+      <div className="flex">
+        <label>
+          <input
+            type="text"
+            className="bg-white border-2 border-gray-300 rounded-lg w-64 h-10 px-4 text-base placeholder-gray-500 text-gray-900 focus:outline-none focus:border-primary"
+            placeholder="Search"
+          />
+          
+      </label>
+      </div>
     </nav>
   );
 };
