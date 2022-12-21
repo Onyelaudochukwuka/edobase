059b11b0f4c8ae8e700f1fcaa21efefa5fbad18c
Udoka <udochukwukaonyela@gmail.com>
2022-12-21 20:47:00 +0100

    created component

--- components/atoms/HomeCard.tsx
+++ components/atoms/HomeCard.tsx
@@ -22,7 +22,14 @@ const HomeCard: FC<IHomeCardProps> = ({
   return (
     <div>
       <div>
-        <Image src={image} alt={title} placeholder="blur" />
+        <Image
+          src={image}
+          width={300}
+          height={200}
+          alt={title}
+          placeholder="blur"
+          blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/5+hHgAHggJ/PchI7wAAAABJRU5ErkJggg=="
+        />
       </div>
       <div>
         <h3>{title}</h3>
