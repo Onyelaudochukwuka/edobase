b9f5a402d5221c0acb3362d9941383e6e60b52ea
Udoka <udochukwukaonyela@gmail.com>
2022-12-22 19:01:59 +0100

    feat: added all properties

--- components/atoms/HomeCard.tsx
+++ components/atoms/HomeCard.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react';
 
 import Image from 'next/image';
+import Link from 'next/link';
 
 export interface IHomeCardProps {
   image: string
   title: string
-  description: string
   uuid: string
   comments: number
   topic: string
@@ -14,30 +14,35 @@ export interface IHomeCardProps {
 const HomeCard: FC<IHomeCardProps> = ({
   image,
   title,
-  description,
   uuid,
   comments,
   topic,
 }) => {
   return (
-    <div className="flex">
-      <div className="basis-7/12">
-        <Image
-          src={image}
-          width={300}
-          height={200}
-          alt={title}
-        />
-      </div>
-      <div>
-        <h3>{title}</h3>
-        <span>Topic</span>
-        <div>
-          <span>12 comments</span>
-          <span>Share</span>
+    <Link href={`/post/${uuid}`}>
+      <div className="flex w-3/4 mx-auto">
+        <div className="basis-7/12">
+          <Image
+            src={image}
+            width={300}
+            height={200}
+            alt={title}
+          />
+        </div>
+        <div className="flex flex-col">
+          <h3>{title}</h3>
+          <span>{topic}</span>
+          <div>
+            <span>
+              {comments}
+              {' '}
+              comments
+            </span>
+            <span>Share</span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 export default HomeCard;
