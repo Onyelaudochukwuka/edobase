f27bfbb9ca8bf5eac7200cc3eb31d81d0787c548
Udoka <udochukwukaonyela@gmail.com>
2022-12-23 04:52:08 +0100

    feat: added ads value

--- components/atoms/SideLink.tsx
+++ components/atoms/SideLink.tsx
@@ -3,5 +3,17 @@ import React, { FC } from 'react';
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ISideLinkProps {}
 
-const SideLink: FC<ISideLinkProps> = () => <section>SideLink</section>;
+const SideLink: FC<ISideLinkProps> = () => {
+  const ads = Array.from({ length: 5 }, (_, i) => i);
+  return (
+    <div className="flex flex-col gap-6">
+      { ads.map((value) => (
+        <div key={value} className="flex flex-col gap-2">
+          Ads
+        </div>
+      ))}
+    </div>
+
+  );
+};
 export default SideLink;
