37f09f0c1d3d9d02c5c774a19529dd8fd218b524
Udoka <udochukwukaonyela@gmail.com>
2022-12-23 04:51:37 +0100

    styling: added classname

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,20 @@
 import React, { FC } from 'react';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ISidebarProps {
   className: string
 }
 
-const Sidebar: FC<ISidebarProps> = ({ className }) => <aside className={`${className}`}>Sidebar</aside>;
+const Sidebar: FC<ISidebarProps> = ({ className }) => {
+  const ads = Array.from({ length: 5 }, (_, i) => i);
+  return (
+    <div className={`${className} flex flex-col gap-6 my-6`}>
+      {ads.map((value) => (
+        <div key={value} className="flex flex-col gap-2 w-56 h-56 items-center justify-center bg-white">
+          Ads
+        </div>
+      ))}
+    </div>
+  );
+};
 export default Sidebar;
