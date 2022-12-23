a62b0dfc0dfab03f5e6e0770e11e9b1b93515d2e
Udoka <udochukwukaonyela@gmail.com>
2022-12-23 07:05:36 +0100

    feat: imported images and responsive styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,18 @@ import React from 'react';
 import type { NextPage } from 'next';
 
 import { HomeCard, Layout } from '../components';
+import img from '../public/image1.png';
 
 const Home: NextPage = () => {
   const posts = Array.from({ length: 5 }, () => ({
     title: 'Creative Mornings',
-    image: '/image1.png',
+    image: img,
     comments: 12,
     topic: 'topic',
     uuid: 'uuid',
   }));
   return (
-    <Layout className="flex gap-6 py-6 flex-col">
+    <Layout className="flex gap-6 py-6 flex-col w-full">
       {posts.map((value) => (
         <HomeCard key={value.uuid} {...value} />
       ))}
