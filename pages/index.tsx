29ab76386f7bcc995bfed8e76c98c16f97262e2e
Udoka <udochukwukaonyela@gmail.com>
2022-12-23 04:51:05 +0100

    created a posts demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,16 @@ import type { NextPage } from 'next';
 import { HomeCard, Layout } from '../components';
 
 const Home: NextPage = () => {
+  const posts = Array.from({ length: 5 }, () => ({
+    title: 'Creative Mornings',
+    image: '/image1.png',
+    comments: 12,
+    topic: 'topic',
+    uuid: 'uuid',
+  }));
   return (
-    <Layout className="">
-      {[
-        {
-          title: 'Creative Mornings',
-          image: '/image1.png',
-          comments: 12,
-          topic: 'topic',
-          uuid: 'uuid',
-        },
-      ].map((value) => (
+    <Layout className="flex gap-6 py-6 flex-col">
+      {posts.map((value) => (
         <HomeCard key={value.uuid} {...value} />
       ))}
     </Layout>
