30c2a86fbb55d1e591f4ea0ef326e06b6f82c11a
Udoka <udochukwukaonyela@gmail.com>
2022-12-23 20:55:11 +0100

    feat: created Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 
+import { Ad } from './atoms';
+
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ISidebarProps {
   className: string
@@ -8,12 +10,8 @@ export interface ISidebarProps {
 const Sidebar: FC<ISidebarProps> = ({ className }) => {
   const ads = Array.from({ length: 5 }, (_, i) => i);
   return (
-    <div className={`${className} flex flex-col gap-6 my-6`}>
-      {ads.map((value) => (
-        <div key={value} className="flex flex-col gap-2 w-56 h-56 items-center justify-center bg-white">
-          Ads
-        </div>
-      ))}
+    <div className={`${className} flex flex-col gap-6 m-6 `}>
+      {ads.map((value) => (<Ad key={value} />))}
     </div>
   );
 };
