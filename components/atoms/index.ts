19ccbb7a4354f7296784bff1572214d1a1c56c4d
Udoka <udochukwukaonyela@gmail.com>
2022-12-23 20:50:24 +0100

    chore: exported new components

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -1,3 +1,4 @@
 export { default as Navlink } from './NavLink';
 export { default as PlaceholderInput } from './PlaceholderInput';
 export { default as HomeCard } from './HomeCard';
+export { default as Ad } from './Ad';
