783e1701fe4fd04b756b55bc8ba40160a0a073d9
Udoka <udochukwukaonyela@gmail.com>
2022-12-23 20:50:10 +0100

    styling: made responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import type { NextPage } from 'next';
 
-import { HomeCard, Layout } from '../components';
+import { Ad, HomeCard, Layout } from '../components';
 import img from '../public/image1.png';
 
 const Home: NextPage = () => {
@@ -15,8 +15,15 @@ const Home: NextPage = () => {
   }));
   return (
     <Layout className="flex gap-6 py-6 flex-col w-full">
-      {posts.map((value) => (
-        <HomeCard key={value.uuid} {...value} />
+      {posts.map((value, i) => ((i + 1) % 5 === 0 && i !== 0 ? (
+        <>
+          <HomeCard key={value.uuid} {...value} />
+          <Ad />
+        </>
+      )
+        : (
+          <HomeCard key={value.uuid} {...value} />
+        )
       ))}
     </Layout>
   );
