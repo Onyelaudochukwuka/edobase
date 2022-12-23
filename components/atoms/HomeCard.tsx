37ec2d1011af4b038d8a4a631c648c99e53161b5
Udoka <udochukwukaonyela@gmail.com>
2022-12-23 20:50:49 +0100

    styling: made page responsive

--- components/atoms/HomeCard.tsx
+++ components/atoms/HomeCard.tsx
@@ -3,8 +3,6 @@ import React, { FC } from 'react';
 import Image, { StaticImageData } from 'next/legacy/image';
 import Link from 'next/link';
 
-import { Comment } from '../../assets';
-
 export interface IHomeCardProps {
   image: string | StaticImageData
   title: string
@@ -21,37 +19,43 @@ const HomeCard: FC<IHomeCardProps> = ({
   topic,
 }) => {
   return (
-    <div className="flex gap-6 mx-auto items-center bg-white pr-8 rounded-lg overflow-hidden shadow-xl w-11/12 relative">
-      <div className="relative basis-6/12">
+    <div className="flex gap-8 mx-auto items-center bg-white pr-8 rounded-lg overflow-hidden shadow-xl w-11/12 relative">
+      <div className="relative basis-6/12 lg:scale-100 scale-150">
         <Image
           src={image}
           alt={title}
           layout="responsive"
-          className="w-full h-full object-cover aspect-video"
+          className="w-full h-full object-cover aspect-video "
         />
       </div>
-      <div className="flex flex-col justify-around h-full content-between gap-3 basis-6/12">
-        <Link href={`/post/${uuid}`}>
-          <h3 className="lg:text-3xl text-xl font-bold">{title}</h3>
-        </Link>
-        <span className="bg-gray w-fit px-5 py-1 rounded-full text-xxs lg:text-xs">
-          {topic}
-        </span>
-        <div className="flex justify-between items-center text-sm">
-          <div className="flex gap-2 items-center lg:text-base text-xs relative">
+      <div className="flex flex-col justify-around h-full content-between gap-3 basis-6/12 py-3">
+        <div className="flex flex-col gap-3">
+          <Link href={`/posts/${uuid}`}>
+            <h3 className="lg:text-3xl text-base font-bold no-break">
+              {title}
+            </h3>
+          </Link>
+          <span className="bg-gray w-fit lg:px-5 px-3 py-1 rounded-full text-xxs lg:text-xs">
+            {topic}
+          </span>
+        </div>
+        <div className="flex justify-between items-center lg:text-sm text-xxs">
+          <div className="flex gap-2 items-center relative">
             {/* <Comment />
-            <span className="bg-action text-xxs text-white rounded-full w-3 h-3 grid place-content-center -right-1 -top-1 absolute">
+            // eslint-disable-next-line max-len
+            <span className="bg-action text-xxs
+            text-white rounded-full w-3 h-3 grid place-content-center -right-1 -top-1 absolute">
               {comments}
             </span> */}
             {comments}
             {' '}
             comments
           </div>
-          <span className="text-xs">Share</span>
+          <span className="text-inherit">Share</span>
         </div>
       </div>
-      <div className="absolute right-4 top-8">
-        <div className="w-1.5 h-1.5 bg-gray rounded-full after:w-1.5 after:h-1.5 after:bg-gray after:rounded-full before:w-1.5 before:h-1.5 before:bg-gray before:rounded-full before:absolute after:absolute relative before:-top-3 after:top-3" />
+      <div className="absolute right-4 top-6">
+        <div className="w-1 h-1 bg-gray rounded-full after:w-1 after:h-1 after:bg-gray after:rounded-full before:w-1 before:h-1 before:bg-gray before:rounded-full before:absolute after:absolute relative before:-top-2 after:top-2" />
       </div>
     </div>
   );
