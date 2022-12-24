a1f7922c005039c032bb80b731bd62a2b3a79fb7
Udoka <udochukwukaonyela@gmail.com>
2022-12-24 16:31:49 +0100

    styling: modified postacrd components

--- components/atoms/PostCard.tsx
+++ components/atoms/PostCard.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 
 import moment from 'moment';
+import Link from 'next/link';
 
 export interface IPostCardProps {
   gender: 'M' | 'F'
@@ -8,25 +9,31 @@ export interface IPostCardProps {
   comments: number
   views: number
   title: string
+  uuid: string
+  author: string
 }
 
 const PostCard: FC<IPostCardProps> = ({
-  gender, time, comments, views, title,
+  gender, time, comments, views, title, uuid, author,
 }) => {
   // eslint-disable-next-line no-console
   return (
     <div className="flex bg-white flex-col px-6 py-4 gap-4 w-full rounded-md shadow-xl shadow-black/5">
       <div className="flex flex-col gap-2">
-        <span>
-          Author
-
-          (
-          {gender}
-          )
+        <Link href={`/users/${author}`}>
+          <span className="lg:text-base text-sm hover:underline transition-all ease-in duration-500">
+            Author (
+            {gender}
+            )
+          </span>
+        </Link>
+        <span className="text-xs">
+          {moment(time).format('h:mm • MMMM Do,YYYY')}
         </span>
-        <span className="text-xs">{moment(time).format('h:mm • MMMM Do,YYYY')}</span>
       </div>
-      <h1 className="text-2xl font-bold">{title}</h1>
+      <Link href={`/posts/${uuid}`}>
+        <h1 className="text-2xl font-bold">{title}</h1>
+      </Link>
       <div className="flex justify-between text-xs lg:flex-base">
         <span>
           {comments}
@@ -38,9 +45,7 @@ const PostCard: FC<IPostCardProps> = ({
           {' '}
           views
         </span>
-        <span>
-          Share
-        </span>
+        <span>Share</span>
       </div>
     </div>
   );
