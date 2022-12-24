128532084fbfac5361970ab094303f2a8d515b5c
Udoka <udochukwukaonyela@gmail.com>
2022-12-24 16:31:38 +0100

    styling: modified ads component

--- components/atoms/Ad.tsx
+++ components/atoms/Ad.tsx
@@ -8,9 +8,7 @@ const Ad: FC<IAdProps> = () => {
   return (
     <div
       className="flex flex-col gap-2 w-full m-auto h-56 items-center justify-center bg-white rounded-lg shadow-xl"
-    >
-      Ads
-    </div>
+    />
   );
 };
 export default Ad;
