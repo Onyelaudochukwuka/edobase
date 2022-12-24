b60ba3ca36d9d9a5d7ecb11845a659d2ec1d45c3
Udoka <udochukwukaonyela@gmail.com>
2022-12-24 17:25:04 +0100

    feat: optimizing page details

--- pages/users/[user].tsx
+++ pages/users/[user].tsx
@@ -1,22 +1,40 @@
 import React from 'react';
 
 import type { NextPage } from 'next';
+import Image from 'next/image';
 
 import { Layout } from '../../components';
+import img1 from '../../public/image1.png';
 
-export interface IuserProps {
-  slug: string
+export interface IUserProps {
+  user: string
 }
 
-const user: NextPage<IuserProps> = ({ slug }) => {
+// eslint-disable-next-line react/prop-types
+const User: NextPage<IUserProps> = ({ user }) => {
   return (
-    <Layout className="">
-      user
+    <Layout className="flex gap-6  flex-col  bg-white mx-auto my-6 w-11/12">
+      <div>
+        <span className="w-12 h-12">
+          <Image src={img1} alt={user} width={50} height={50} />
+        </span>
+        <div>
+          <span>{user}</span>
+          <span>Male</span>
+        </div>
+      </div>
+      <div>
+        <span>
+          {' '}
+          18 years old | Full Stack Developer | Open source enthusiast | UI
+          designer | follow for consistency and productivity tips.
+        </span>
+      </div>
     </Layout>
   );
 };
-user.getInitialProps = async ({ query }: any) => {
-  const { slug } = query;
-  return { slug };
+User.getInitialProps = async ({ query }: any) => {
+  const { user } = query as { user: string };
+  return { user };
 };
-export default user;
+export default User;
