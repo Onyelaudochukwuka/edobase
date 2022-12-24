208648d0b99fec25d7a6f49e5c042e4cc241704f
Udoka <udochukwukaonyela@gmail.com>
2022-12-24 15:46:14 +0100

    styling: width fixes

--- components/atoms/Ad.tsx
+++ components/atoms/Ad.tsx
@@ -7,7 +7,7 @@ export interface IAdProps {
 const Ad: FC<IAdProps> = () => {
   return (
     <div
-      className="flex flex-col gap-2 w-11/12 m-auto h-56 items-center justify-center bg-white rounded-lg shadow-xl"
+      className="flex flex-col gap-2 w-full m-auto h-56 items-center justify-center bg-white rounded-lg shadow-xl"
     >
       Ads
     </div>
