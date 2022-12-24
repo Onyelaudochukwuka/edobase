1b1a52057496a5b67f1371284003d43bceb6be96
Udoka <udochukwukaonyela@gmail.com>
2022-12-24 15:12:03 +0100

    feat: added cards

--- pages/forums/[forum].tsx
+++ pages/forums/[forum].tsx
@@ -2,24 +2,35 @@ import React from 'react';
 
 import type { NextPage } from 'next';
 
-import { Layout } from '../../components';
+import { Layout, PostCard } from '../../components';
+import { IPostCardProps } from '../../components/atoms/PostCard';
 
-export interface IForumProps {
+interface IForumProps {
   forum: string
 }
 
+// eslint-disable-next-line react/prop-types
 const Forum: NextPage<IForumProps> = ({ forum }) => {
+  const posts: IPostCardProps[] = Array.from({ length: 10 }, () => ({
+      comments: 0,
+      gender: 'M',
+      time: Date.now(),
+      title: 'Post 1',
+      views: 10,
+    }));
   return (
-    <Layout className="">
-      <div className="flex">
-        <h1 className="text-xl">{`Forums/${forum}`}</h1>
-        <span className="text-lg">Create A topic</span>
+    <Layout className="p-6 w-full">
+      <div className="flex gap-4">
+        <h1 className="text-3xl">{`Forums/${forum}`}</h1>
+        {
+          posts.map((value) => <PostCard {...value} />)
+        }
       </div>
     </Layout>
   );
 };
 Forum.getInitialProps = async ({ query }: any) => {
-  const { forum } = query;
+  const { forum } = query as { forum: string };
   return { forum };
 };
 export default Forum;
