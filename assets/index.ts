b784eab82bb9b6086936ea569373717002ee7ec7
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 12:35:51 +0100

    feqat: exported icon

--- assets/index.ts
+++ assets/index.ts
@@ -1,8 +1,9 @@
-export { default as Home } from './Home.icon';
-export { default as AuthPointer } from './AuthPointer.icon';
-export { default as AuthBlur } from './AuthBlur.icon';
-export { default as Check } from './Check.icon';
-export { default as Key } from './Key.icon';
+export { default as Add } from './Add.icon';
 export { default as Arrow } from './Arrow.icon';
-export { default as Search } from './Search.icon';
+export { default as AuthBlur } from './AuthBlur.icon';
+export { default as AuthPointer } from './AuthPointer.icon';
+export { default as Check } from './Check.icon';
 export { default as Comment } from './Comment.icon';
+export { default as Home } from './Home.icon';
+export { default as Key } from './Key.icon';
+export { default as Search } from './Search.icon';
