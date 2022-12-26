e0ac612804eb651570e95cef538013b4aac2b1a7
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 08:06:37 +0100

    feat:: added comments

--- pages/posts/[post].tsx
+++ pages/posts/[post].tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import type { NextPage } from 'next';
 
-import { Layout, PostView } from '../../components';
+import { Layout, PostView, Comments } from '../../components';
+import type { ICommentsProps } from '../../components/Comments';
 import type { IPostViewProps } from '../../components/PostView';
 
 export interface IPostProps {
@@ -22,9 +23,22 @@ const Post: NextPage<IPostProps> = ({ slug }) => {
     dislikes: 50,
     gender: 'M',
   };
+  const comments: ICommentsProps[] = Array.from({ length: 10 }, (_, i) => ({
+    reply: 'string',
+    author: 'string',
+    comment:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus voluptate unde! Inventore tempore rem nobis aut natus. Esse nobis facere quos error placeat voluptas cumque at tempora reprehenderit ipsum.',
+    likes: 10 * i,
+    dislikes: 10,
+    time: Date.now(),
+  }));
+
   return (
     <Layout className="flex gap-6 flex-col mx-auto my-6 w-11/12">
       <PostView {...post} />
+      {
+        comments.map((value) => (<Comments {...value} />))
+      }
     </Layout>
   );
 };
