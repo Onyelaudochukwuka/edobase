08a6428361b86381865c6ec07a9b89e7bc0469de
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 12:36:10 +0100

    feat: modified properties and removed vertical margin

--- components/atoms/Ad.tsx
+++ components/atoms/Ad.tsx
@@ -7,7 +7,7 @@ export interface IAdProps {
 const Ad: FC<IAdProps> = () => {
   return (
     <div
-      className="flex flex-col gap-2 w-full m-auto h-56 items-center justify-center bg-white rounded-lg shadow-xl"
+      className="flex flex-col gap-2 w-full mx-auto h-56 items-center justify-center bg-white rounded-lg shadow-xl"
     >
       Ad
     </div>
