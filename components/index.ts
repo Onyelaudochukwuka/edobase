09db80e05d3568081d4250d24ab1abb55a95ee83
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 08:06:05 +0100

    feat: exported menu panel

--- components/index.ts
+++ components/index.ts
@@ -5,4 +5,5 @@ export { default as Layout } from './Layout';
 export { default as Footer } from './Footer';
 export { default as Sidebar } from './Sidebar';
 export { default as PostView } from './PostView';
+export { default as Comments } from './Comments';
 export * from './atoms';
