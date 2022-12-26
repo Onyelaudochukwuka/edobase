002f15357490090de050908d0c3741717f576d87
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 12:39:26 +0100

    feat: formatted

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 
+import Link from 'next/link';
+
 import { Ad } from './atoms';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
@@ -10,8 +12,18 @@ export interface ISidebarProps {
 const Sidebar: FC<ISidebarProps> = ({ className }) => {
   const ads = Array.from({ length: 5 }, (_, i) => i);
   return (
-    <div className={`${className} flex flex-col gap-6 m-6`}>
-      {ads.map((value) => (<Ad key={value} />))}
+    <div className={`${className} flex flex-col gap-6 p-6`}>
+      <button
+        type="button"
+        className="px-6 w-fit py-2 bg-action mx-auto hover:bg-basic text-white rounded-full transition-colors duration-500 ease-in"
+      >
+        <Link href="/create-post">
+          Create Post
+        </Link>
+      </button>
+      {ads.map((value) => (
+        <Ad key={value} />
+      ))}
     </div>
   );
 };
