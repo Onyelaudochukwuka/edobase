a22fadf2401e5d68c18890b71014723291120e22
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 11:17:28 +0100

    feat: created editor components

---
+++ components/Editor.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react';
+
+import ReactQuill from 'react-quill';
+
+export interface IEditorProps {
+  value: string
+  setValue: (value: string) => void
+}
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+    ['link', 'image'],
+    ['clean'],
+  ],
+};
+const formats = [
+  'header',
+  'bold', 'italic', 'underline', 'strike', 'blockquote',
+  'list', 'bullet', 'indent',
+  'link', 'image',
+];
+const Editor: FC<IEditorProps> = ({ value, setValue }) => {
+  return (
+    <ReactQuill
+      theme="snow"
+      value={value}
+      onChange={setValue}
+      className="bg-white"
+      modules={modules}
+      formats={formats}
+    />
+  );
+};
+export default Editor;
