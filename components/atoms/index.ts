08318d87dd92f27770ab875b414a084075215c86
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 11:18:49 +0100

    feat: created component export

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -3,3 +3,4 @@ export { default as PlaceholderInput } from './PlaceholderInput';
 export { default as HomeCard } from './HomeCard';
 export { default as Ad } from './Ad';
 export { default as PostCard } from './PostCard';
+export { default as CommentEditor } from './CommentEditor';
