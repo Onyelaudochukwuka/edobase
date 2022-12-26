c32d8160c5722afd05ae62ea0cecf14603d3d09f
Udoka <udochukwukaonyela@gmail.com>
2022-12-26 08:06:23 +0100

    feat: styling links

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,48 +1,54 @@
 import React, { FC } from 'react';
 
+import moment from 'moment';
+
 export interface ICommentsProps {
   reply: string
   author: string
   comment: string
   likes: number
   dislikes: number
+  time: number
 }
 
 const Comments: FC<ICommentsProps> = ({
-  reply, author, comment, likes, dislikes,
+  reply, author, comment, likes, dislikes, time,
 }) => {
   return (
-    <div>
-      <h3>
-        RE:
-        {' '}
-        <span className="text-primary">
-          @
-          {reply}
+    <div className="bg-white w-full p-6 flex flex-col gap-4 rounded-md shadow-2xl shadow-black/5">
+      <div>
+        <h3>
+          RE:
+          {' '}
+          <span className="text-primary">
+            @
+            {reply}
+          </span>
+        </h3>
+        <span className="text-xs font-semibold">
+          {moment(time).format('h:mm • MMMM Do,YYYY')}
         </span>
-      </h3>
-      <h4>{author}</h4>
-      <p>
-        {comment}
-      </p>
-      <div className="flex gap-4 text-xs font-semibold">
-        <span>
+      </div>
+      <h4 className="font-bold text-lg">{author}</h4>
+      <p>{comment}</p>
+      <div className="flex gap-4 font-medium w-full justify-between border-t-2 border-gray pt-4 lg:text-xs text-xxs">
+        <span className="hover:underline transition-all ease-in duration-500 cursor-pointer">
           {likes}
           {' '}
           likes
         </span>
-        <span>
+        <span className="hover:underline transition-all ease-in duration-500 cursor-pointer">
           {dislikes}
           {' '}
           dislikes
         </span>
-        <span>
+        <span className="hover:underline transition-all ease-in duration-500 cursor-pointer">
           Quote
         </span>
-        <span>
+        <span className="hover:underline transition-all ease-in duration-500 cursor-pointer">
           Share
         </span>
-        <span>
+        <span className="hover:underline transition-all ease-in duration-500 cursor-pointer">
           Report
         </span>
       </div>
