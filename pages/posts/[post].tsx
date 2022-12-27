e25ffe52b7d7ff363440d4a5a625bb7da76b671e
Udoka <udochukwukaonyela@gmail.com>
2022-12-27 17:24:31 +0100

    feat: added scroll property

--- pages/posts/[post].tsx
+++ pages/posts/[post].tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 
+import { motion, useScroll, useSpring } from 'framer-motion';
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 
-import {
-  Layout, PostView, Comments,
-} from '../../components';
+import { Layout, PostView, Comments } from '../../components';
 import type { ICommentsProps } from '../../components/Comments';
 import type { IPostViewProps } from '../../components/PostView';
 
@@ -18,11 +17,19 @@ const DynamicComponentWithNoSSR = dynamic(
 );
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, react/prop-types
 const Post: NextPage<IPostProps> = ({ slug }) => {
+  const { scrollYProgress } = useScroll();
+  const { div: Div } = motion;
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   const [value, setValue] = useState('');
   const post: IPostViewProps = {
     author: 'string',
     title: 'THIS IS A TITLE',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus voluptate unde! Inventore tempore rem nobis aut natus. Esse nobis facere quos error placeat voluptas cumque at tempora reprehenderit ipsum.',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus voluptate unde! Inventore tempore rem nobis aut natus. Esse nobis facere quos error placeat voluptas cumque at tempora reprehenderit ipsum.',
     comments: 50,
     views: 1159,
     time: Date.now(),
@@ -42,6 +49,10 @@ const Post: NextPage<IPostProps> = ({ slug }) => {
 
   return (
     <Layout hide className="flex gap-6 flex-col mx-auto my-6 w-11/12">
+      <Div
+        className="fixed inset-x-0 top-0 h-3 bg-action origin-[0%]"
+        style={{ scaleX }}
+      />
       <PostView {...post} />
       <DynamicComponentWithNoSSR {...{ value, setValue }} />
       {comments.map((props) => (
