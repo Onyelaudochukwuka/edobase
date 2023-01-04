ab032c8850790e6ddd371db7f052e5b4a1e7679a
Udoka <udochukwukaonyela@gmail.com>
2023-01-04 19:07:54 +0100

    feat: styling

--- components/atoms/HomeCard.tsx
+++ components/atoms/HomeCard.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-import Image, { StaticImageData } from 'next/legacy/image';
+import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
 
 export interface IHomeCardProps {
@@ -8,7 +8,6 @@ export interface IHomeCardProps {
   title: string
   uuid: string
   comments: number
-  topic: string
 }
 
 const HomeCard: FC<IHomeCardProps> = ({
@@ -16,29 +15,22 @@ const HomeCard: FC<IHomeCardProps> = ({
   title,
   uuid,
   comments,
-  topic,
 }) => {
   return (
-    <div className="flex gap-8 mx-auto items-center bg-white pr-8 rounded-lg overflow-hidden shadow-xl w-full relative">
-      <div className="relative basis-6/12 md:scale-100 scale-150 overflow-hidden">
+    <div className="flex flex-col gap-2 mx-auto items-center bg-white rounded-lg overflow-hidden shadow-xl w-full relative h-fit">
+      <div className="relative w-full overflow-hidden">
         <Image
           src={image}
           alt={title}
-          layout="responsive"
-          className="w-full h-full object-cover aspect-video "
+          className="w-full h-full object-cover"
         />
       </div>
-      <div className="flex flex-col justify-around h-full content-between gap-3 basis-6/12 py-3">
-        <div className="flex flex-col gap-3">
-          <Link href={`/posts/${uuid}`}>
-            <h3 className="lg:text-3xl text-base font-bold no-break">
-              {title}
-            </h3>
-          </Link>
-          <span className="bg-gray w-fit lg:px-5 px-3 py-1 rounded-full text-xxs lg:text-xs">
-            {topic}
-          </span>
-        </div>
+      <div className="flex flex-col content-between gap-3 py-3">
+        <Link href={`/posts/${uuid}`}>
+          <h3 className="lg:text-3xl text-base font-bold no-break">
+            {title}
+          </h3>
+        </Link>
         <div className="flex justify-between items-center lg:text-sm text-xxs">
           <div className="flex gap-2 items-center relative">
             {/* <Comment />
@@ -55,7 +47,7 @@ const HomeCard: FC<IHomeCardProps> = ({
         </div>
       </div>
       <div className="absolute right-4 top-6">
-        <div className="w-1 h-1 bg-gray rounded-full after:w-1 after:h-1 after:bg-gray after:rounded-full before:w-1 before:h-1 before:bg-gray before:rounded-full before:absolute after:absolute relative before:-top-2 after:top-2" />
+        <div className="w-1 h-1 bg-white rounded-full after:w-1 after:h-1 after:bg-white after:rounded-full before:w-1 before:h-1 before:bg-white before:rounded-full before:absolute after:absolute relative before:-top-2 after:top-2" />
       </div>
     </div>
   );
