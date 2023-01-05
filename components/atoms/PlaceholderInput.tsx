39f47abd5195d011514b571e341feca9fcea0f9c
Udoka <udochukwukaonyela@gmail.com>
2023-01-05 22:04:51 +0100

    feat: added new type interface

--- components/atoms/PlaceholderInput.tsx
+++ components/atoms/PlaceholderInput.tsx
@@ -10,6 +10,7 @@ interface IPlaceholderInputProps<T> {
   placeholder: InputHTMLAttributes<HTMLInputElement>['placeholder']
   state: InputHTMLAttributes<HTMLInputElement>['value']
   setState: Dispatch<SetStateAction<T>>
+  inputMode: InputHTMLAttributes<HTMLInputElement>['inputMode']
 }
 
 const PlaceholderInput = <T extends unknown>({
@@ -17,6 +18,7 @@ const PlaceholderInput = <T extends unknown>({
   placeholder,
   state,
   setState,
+  inputMode,
   ...props
 }: IPlaceholderInputProps<T>): ReactElement => (
   <input
@@ -28,6 +30,7 @@ const PlaceholderInput = <T extends unknown>({
       onChange: (e) => setState((typeof state === 'number' ? Number(e.target.value) : e.target.value) as T),
     }}
     className="border-b-2 outline-none focus:outline-none border-action/50 focus:border-action/100 transition-colors duration-300 ease-in px-1 py-3"
+    inputMode={inputMode}
   />
   );
 
