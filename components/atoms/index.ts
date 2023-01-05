b30680188ef37748ef467386aa10669077495098
Udoka <udochukwukaonyela@gmail.com>
2023-01-05 22:01:40 +0100

    feat: exported atoms

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -4,3 +4,4 @@ export { default as HomeCard } from './HomeCard';
 export { default as Ad } from './Ad';
 export { default as PostCard } from './PostCard';
 export { default as CommentEditor } from './CommentEditor';
+export { default as SelectInput } from './SelectInput';
