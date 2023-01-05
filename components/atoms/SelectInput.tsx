285179a0e89c12f19ff898f2722336b7bb4cc169
Udoka <udochukwukaonyela@gmail.com>
2023-01-05 22:02:30 +0100

    feta: created select input component

--- components/atoms/SelectInput.tsx
+++ components/atoms/SelectInput.tsx
@@ -3,20 +3,34 @@ import React, {
 } from 'react';
 
 export interface ISelectInputProps {
-  values: string
+  values: string[]
   state: InputHTMLAttributes<HTMLSelectElement>['value']
   setState: Dispatch<SetStateAction<string>>
   placeholder: string
 }
 
-const SelectInput: FC<ISelectInputProps> = (props) => {
+const SelectInput: FC<ISelectInputProps> = ({
+  values, state, setState, placeholder, ...props
+}) => {
   return (
     <section>
       <select
         {...props}
-        className="border-b-2 outline-none focus:outline-none border-action/50 focus:border-action/100 transition-colors duration-300 ease-in px-1 py-3"
+        placeholder={placeholder}
+        value={state}
+        onChange={(e) => setState(e.target.value)}
+        className={`border-b-2 outline-none focus:outline-none border-action/50 focus:border-action/100 transition-colors duration-300 ease-in px-1 py-3 ${
+          state === '' ? 'text-[#737070]' : 'text-inherit'
+        }`}
       >
-        <option value="1">1</option>
+        <option value="" disabled>
+          {placeholder}
+        </option>
+        {values.map((value) => (
+          <option value={value} key={value}>
+            {value}
+          </option>
+        ))}
       </select>
     </section>
   );
