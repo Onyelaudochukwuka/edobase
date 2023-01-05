8fe807cc0d9b69f7931d722b633405dc9bd2e202
Udoka <udochukwukaonyela@gmail.com>
2023-01-05 22:03:16 +0100

    feat: added new helper for local storage

--- utils/functions/helpers.ts
+++ utils/functions/helpers.ts
@@ -16,3 +16,33 @@ export function hasMinimumLength(value: string, length: number): boolean {
 export function isEmail(value: string): boolean {
   return value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/) !== null;
 }
+
+export function getWithExpiry(key: string): string | null {
+  const itemStr = localStorage.getItem(key) ?? '';
+  // if the item doesn't exist, return null
+  if (itemStr === '') {
+    return null;
+  }
+  const item = JSON.parse(itemStr);
+  const now = new Date();
+  // compare the expiry time of the item with the current time
+  if (now.getTime() > item.expiry) {
+    // If the item is expired, delete the item from storage
+    // and return null
+    localStorage.removeItem(key);
+    return null;
+  }
+  return item.value;
+}
+
+export function setWithExpiry(key: string, value: string, ttl: number): void {
+  const now = new Date();
+
+  // `item` is an object which contains the original value
+  // as well as the time when it's supposed to expire
+  const item = {
+    value,
+    expiry: now.getTime() + ttl,
+  };
+  localStorage.setItem(key, JSON.stringify(item));
+}
